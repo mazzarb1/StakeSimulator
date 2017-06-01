0f7e599485391bc6f9703462aae0488957a95014
Brandon Mazzarella <mazzarb1@tcnj.edu>
2017-06-01 16:20:01 -0400

    Updated
    Update fixes bugs and works flawlessly.  
    
    Added counter for highest streak, highest amount had, and how many until you lost.

--- StakeCalculator.java
+++ StakeCalculator.java
@@ -11,56 +11,76 @@ public class StakeCalculator
 
         int startingStack = 50;
         int stakeNumber = 1;
-        int higestCashStack = startingStack;
+        int cashHigh = 50;
 
-    for(int i = 0; i < 1000; i++) {
-        if (stakeNumber == 1) {
+        boolean won = true;
+        int winStreak = 0;
+        int highestWin = 0;
+        int loseStreak = 0;
+
+
+        for(int i = 1; i <= 1000; i++) {
             int n = rand.nextInt(2) + 1;
-            if (n == 1) {
+
+            if(won)
+            {
+                winStreak = winStreak + 1;
+            }
+
+            if(startingStack > cashHigh)
+            {
+                cashHigh = startingStack;
+            }
+
+            if(highestWin < winStreak)
+            {
+                highestWin = winStreak;
+            }
+
+            if(startingStack < 1)
+            {
+                System.out.println("");
+                System.out.println("You ran out of money on round: " + (i-1));
+                i = 10000;
+
+            }
+
+
+            if(stakeNumber == 1 && n == 1)
+            {
                 startingStack = startingStack + 1;
 
-            } else {
+            }else if (stakeNumber == 1 && n == 2)
+            {
                 startingStack = startingStack - 1;
                 stakeNumber = 2;
-            }
-
-        }
-        if (stakeNumber == 2) {
-            int n = rand.nextInt(2) + 1;
-            if (n == 1) {
+                winStreak = 0;
+            }else if(stakeNumber == 2 && n == 1)
+            {
                 startingStack = startingStack + 1;
                 stakeNumber = 1;
-
-            } else {
+            }else if (stakeNumber == 2 && n == 2 && startingStack >= 6)
+            {
                 startingStack = startingStack - 1;
                 stakeNumber = 3;
+                winStreak = 0;
+            }else if (stakeNumber == 3 && n == 1)
+            {
+                startingStack = startingStack + 5;
+                stakeNumber = 1;
+
+            }else if (stakeNumber == 3 && n == 2) {
+                startingStack = startingStack - 5;
+                stakeNumber = 1;
+                winStreak = 0;
+            }
+
+            if(i < 1000) {
+                System.out.println("Stake Round: " + i + " Ending Cash Stack: " + startingStack);
             }
         }
-            if(stakeNumber == 3)
-            {
-                int n = rand.nextInt(2) + 1;
-                if(n == 1)
-                {
-                    startingStack = startingStack + 5;
-                    stakeNumber = 1;
-                }else
-                {
-
-                    startingStack = startingStack - 5;
-                    stakeNumber = 1;
-                }
-
-            }
-            if(higestCashStack < startingStack)
-            {
-                higestCashStack = startingStack;
-            }
-            System.out.println("Stake Number: " + i + " Current Cash Stack: " + startingStack);
-
-
-
-    }
-            System.out.println("The highest your cash stack was: " + higestCashStack);
+            System.out.println("The highest amount of money you had was: " + cashHigh);
+            System.out.println("Your highest winning streak was: " + highestWin);
     }
 
 }
